87f4ac9b94fa6d42f4e109caab703db6227bbd63
Adam Quinton <quintonish@gmail.com>
2020-09-12 20:34:31 -0600

    readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Setup
+`git clone https://github.com/zvakanaka/pi-api.git`  
+`cd pi-api`  
+`npm install`  
+`npm start`  
+
+# Routes
+## `/temperature`
+Returns CPU temperature data. 
+```json
+{
+  celsius: 37,
+  fahrenheit: 98.6
+}
+```
--- routes/temperature.js
+++ routes/temperature.js
@@ -1,6 +1,6 @@
 const { read } = require('../lib/fsPromises')
 
-module.exports = function temperature(req) {
+module.exports = async function temperature(req) {
   const thermalInFileName = '/sys/class/thermal/thermal_zone0/temp'
   const thermalFileContents = await read(thermalInFileName)
   const celsius = Number(thermalFileContents) / 1000
